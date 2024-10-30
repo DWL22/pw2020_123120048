af8fedab22bcc9fa88a07db6aa469888b4ea4134
DWL22 <candra.icy@gmail.com>
2024-10-30 11:18:50 +0800

    koneksi dan tampil data

---
+++ kuliah/pertemuan10/functions.php
@@ -0,0 +1,28 @@
+<?php 
+function koneksi() {
+   return mysqli_connect('localhost', 'root', '', 'phpdasar');
+}
+
+function query($query) {
+    $conn = koneksi();
+    $result = mysqli_query($conn, $query);
+       // Cek apakah query berhasil dijalankan
+    //    if (!$result) {
+    //     die("Query gagal: " . mysqli_error($conn));
+    // }
+
+//jika hanya satu data
+if( mysqli_num_rows($result)== 1){
+    return mysqli_fetch_assoc($result);
+}
+
+
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+$rows[] = $row;
+}
+
+
+return $rows;
+}
+?>
